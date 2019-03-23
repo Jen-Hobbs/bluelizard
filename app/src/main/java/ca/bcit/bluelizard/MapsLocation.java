bb3a42eeff118264950e2585782d1b58df2c7cc4
Jen-Hobbs <jen.g.hobbs@gmail.com>
2019-03-23 16:47:51 -0700

    maps setup show square

--- app/src/main/java/ca/bcit/bluelizard/MapsLocation.java
+++ app/src/main/java/ca/bcit/bluelizard/MapsLocation.java
@@ -1,5 +1,6 @@
 package ca.bcit.bluelizard;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
 
 public class MapsLocation extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,8 +45,29 @@ public class MapsLocation extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng newWest = new LatLng(49.193788,-122.9314024);
+       // List<Double> location = new List<Double>();
+
+        /*
+                [ -122.90167911202795, 49.210838475275544 ],
+						[ -122.90108615046643, 49.211157364714765 ],
+						[ -122.90119873560481, 49.2133490365784 ],
+						[ -122.90169404851203, 49.21394149278283 ],
+						[ -122.90855629898924, 49.21939798924417 ],
+						[ -122.91193355755044, 49.21757116319086 ],
+						[ -122.90406701107361, 49.21131726041395 ],
+						[ -122.90299907265259, 49.21189067262306 ],
+						[ -122.90167911202795, 49.210838475275544 ]
+        */
+
+        mMap.addMarker(new MarkerOptions().position(newWest).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(newWest));
+        /*
+        mMap.addPolygon(
+                new PolygonOptions().add(
+                        sydney, new LatLng(-35, 151), new LatLng(-35, 150), new LatLng(-34, 150)
+                ).fillColor(Color.GREEN)
+        );
+        */
     }
 }
